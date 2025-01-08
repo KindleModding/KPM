7b121e16ea1077d7270669323627f2c3507b0d99
ThatHackerDudeFromCyberspace <69104218+ThatHackerDudeFromCyberspace@users.noreply.github.com>
2025-01-08 11:00:19 +0000

    Add scripts to build packages and add them to repos

---
+++ kpbuild.py
@@ -0,0 +1,62 @@
+import argparse
+import pathlib
+import json
+import tarfile
+from os import path
+import os
+
+parser = argparse.ArgumentParser("KPBuild", description="Kindle Package Builder")
+parser.add_argument("folderpath", type=pathlib.Path)
+
+args = parser.parse_args()
+packagePath = args.folderpath
+
+# Check if the manifest.json file exists
+if (not path.exists(path.join(packagePath, "manifest.json"))):
+    print("manifest.json NOT FOUND - ABORTING!")
+    print("Ensure a manifest.json file exists in the root of your package")
+    exit(1)
+
+if (not path.exists(path.join(packagePath, "icon.png"))):
+    print("icon.png NOT FOUND - ABORTING!")
+    print("Ensure your package has a valid icon.png file")
+    exit(1)
+
+if (not path.exists(path.join(packagePath, "screenshots/"))):
+    print("WARNING: Your package has no screenshots")
+
+buildData = {}
+with open(path.join(packagePath, "manifest.json"), 'r', encoding='utf-8') as file:
+    buildData = json.loads(file.read())
+
+if ("id" not in buildData.keys()):
+    print("Error: manifest.json malformed (missing 'id' key)!")
+
+print("Building Package:")
+print(f"id: [{buildData["id"]}]")
+print(f"name: [{buildData["display_name"]}]")
+print(f"version: [{buildData["version_name"]}]")
+print(f"arch: [{buildData["supported_arch"]}]")
+
+if ("install" in buildData.keys()):
+    if (not path.exists(path.join(packagePath, buildData["install"]))):
+        print(f"Error: Invalid install script specified {buildData["install"]} (NOT_FOUND)")
+        exit(1)
+
+if ("uninstall" in buildData.keys()):
+    if (not path.exists(path.join(packagePath, buildData["uninstall"]))):
+        print(f"Error: Invalid uninstall script specified {buildData["uninstall"]} (NOT_FOUND)")
+        exit(1)
+
+if ("mount" in buildData.keys()):
+    for dest in buildData["mount"]:
+        if (not path.exists(path.join(packagePath, buildData["mount"][dest]))):
+            print(f"Error: Invalid mount location detected {buildData["mount"][dest]} (NOT FOUND)")
+
+print("\n\n")
+
+print("Writing package file")
+tar = tarfile.open(packagePath.parents[0].joinpath(buildData["id"] + '_' + buildData["version_name"] + '_' + buildData["supported_arch"] + '.kpkg'), 'x:gz')
+for item in os.listdir(packagePath):
+    tar.add(path.abspath(path.join(packagePath, item)), arcname=item)
+tar.close()
---
+++ kprepoman.py
@@ -0,0 +1,132 @@
+import argparse
+import pathlib
+import json
+import copy
+import tarfile
+from os import path
+import os
+import shutil
+import getpass
+
+parser = argparse.ArgumentParser("KPBuild", description="Kindle Package Builder")
+parser.add_argument("repopath", type=pathlib.Path)
+parser.add_argument("operator", choices=["init", "add_package", "remove_package"])
+parser.add_argument("target", type=pathlib.Path, nargs='?', default=None)
+
+args = parser.parse_args()
+
+if (args.operator == "init"):
+    print("Initialising repository...")
+    manifest  = {
+        "version": 1,
+        "id": "com." + getpass.getuser() + ".repo",
+        "packages": []
+    }
+    repo_id = input(f"Please enter the ID of your repository ({manifest["id"]})")
+    if (repo_id != ""):
+        manifest["id"] = repo_id
+    
+    with open(args.repopath.joinpath("manifest.json"), 'w', encoding='utf-8') as file:
+        file.write(json.dumps(manifest))
+    print(f"Created repo {manifest["id"]}")
+elif (args.operator == "add_package"):
+    if (args.target == None):
+        print("Error: You must specify a package file to add")
+        exit(1)
+
+    # Check if repo has a manifest
+    repoManifest = {}
+    with open(args.repopath.joinpath("manifest.json"), 'r', encoding='utf-8') as file:
+        repoManifest = json.loads(file.read())
+
+    kpkg = tarfile.open(args.target)
+    # Check if package is valid
+    requiredPaths = ["icon.png", "manifest.json"]
+    for member in kpkg.getmembers():
+        try:
+            requiredPaths.remove(member.path)
+        except:
+            pass
+    if (len(requiredPaths) > 0):
+        print("ERR: The package archive is missing the following files:")
+        print(requiredPaths)
+        exit(1)
+
+    packageManifestFile = kpkg.extractfile("manifest.json")
+    packageManifest = json.load(packageManifestFile)
+
+    # Check if this package already exists in the repo manifest
+    packageIndex = -1
+    for i in range(len(repoManifest["packages"])):
+        if (repoManifest["packages"][i]["id"] == packageManifest["id"]):
+            packageIndex = i
+            break
+
+    if (packageIndex == -1):
+        repoManifest["packages"].append({
+            "id": packageManifest["id"],
+            "alias": packageManifest["alias"],
+            "display_name": packageManifest["display_name"],
+            "description": packageManifest["description"],
+            "screenshots": 0,
+            "versions": []
+        })
+        packageIndex = len(repoManifest["packages"]) - 1
+    else:
+        print("* Package already exists - Updating info")
+        repoManifest["packages"][packageIndex]["display_name"] = packageManifest["display_name"]
+        repoManifest["packages"][packageIndex]["description"] = packageManifest["description"]
+        repoManifest["packages"][packageIndex]["screenshots"] = 0
+
+    print("* Checking version")
+    versionIndex = -1
+    for version in repoManifest["packages"][packageIndex]["versions"]:
+        if (version["version_name"] == packageManifest["version_name"] and version["version_number"] != packageManifest["version_number"]):
+            print(f"ERR: Version name {version["version_name"]} is already defined with version number {version["version_number"]} (tried to add as {version["version_name"]})")
+            exit(1)
+
+        if (version["version_name"] != packageManifest["version_name"] and version["version_number"] == packageManifest["version_number"]):
+            print(f"ERR: Version number {version["version_number"]} is already defined with version name {version["version_name"]} (tried to add as {version["version_number"]})")
+            exit(1)
+
+        if (version["version_name"] == packageManifest["version_name"] and version["version_number"] == packageManifest["version_number"]):
+            print(f"WARN: Version {version["version_name"]} ({version["version_number"]}) already in repo - OVERWRITING")
+
+    if (versionIndex == -1):
+        repoManifest["packages"][packageIndex]["versions"].append({
+            "version_name": packageManifest["version_name"],
+            "version_number": packageManifest["version_number"],
+            "min_firmware": packageManifest["min_firmware"],
+            "max_firmware": packageManifest["max_firmware"],
+            "dependencies": packageManifest["dependencies"],
+            "supported_arch": packageManifest["supported_arch"]
+        })
+    else:
+        repoManifest["packages"][packageIndex]["versions"]["version_name"] = packageManifest["version_name"]
+        repoManifest["packages"][packageIndex]["versions"]["version_number"] = packageManifest["version_number"]
+        repoManifest["packages"][packageIndex]["versions"]["min_firmware"] = packageManifest["min_firmware"]
+        repoManifest["packages"][packageIndex]["versions"]["max_firmware"] = packageManifest["max_firmware"]
+        repoManifest["packages"][packageIndex]["versions"]["dependencies"] = packageManifest["dependencies"]
+        repoManifest["packages"][packageIndex]["versions"]["supported_arch"] = packageManifest["supported_arch"]
+    
+    print("* Creating folder")
+    os.makedirs(args.repopath.joinpath("packages").joinpath(packageManifest["id"]).joinpath(packageManifest["version_name"]), exist_ok=True)
+    print("* Copying icon")
+    kpkg.extract("icon.png", args.repopath.joinpath("packages").joinpath(packageManifest["id"]))
+
+    try:
+        shutil.rmtree(args.repopath.joinpath("packages").joinpath(packageManifest["id"]).joinpath("screenshots"))
+    except:
+        pass
+    os.makedirs(args.repopath.joinpath("packages").joinpath(packageManifest["id"]).joinpath("screenshots"))
+    repoManifest["packages"][packageIndex]["screenshots"] = 0
+
+    for member in kpkg.getmembers():
+        if (member.path[:12] == "screenshots/"):
+            kpkg.extract(member, args.repopath.joinpath("packages").joinpath(packageManifest["id"]))
+            os.rename(args.repopath.joinpath("packages").joinpath(packageManifest["id"]).joinpath(member.path), args.repopath.joinpath("packages").joinpath(packageManifest["id"]).joinpath(f"screenshots/{repoManifest["packages"][packageIndex]["screenshots"]}.png"))
+            repoManifest["packages"][packageIndex]["screenshots"] += 1
+
+    print("* Writing repo manifest")
+    with open(args.repopath.joinpath("manifest.json"), 'w', encoding='utf-8') as file:
+        file.write(json.dumps(repoManifest))
