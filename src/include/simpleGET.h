3ef4590ff681d4aef994ef0aeea89b9536dcee57
ThatHackerDudeFromCyberspace <69104218+ThatHackerDudeFromCyberspace@users.noreply.github.com>
2025-08-08 12:08:34 +0100

    Implement simple CURL getter

---
+++ src/include/simpleGET.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <curl/curl.h>
+
+class SimpleGET
+{
+public:
+    SimpleGET(const char* url);
+    ~SimpleGET();
+
+    CURLcode Perform();
+    char* GetBuffer();
+    size_t GetSize();
+private:
+    static size_t write_callback(char* ptr, size_t size, size_t nmemb, void* userdata);
+    CURL* curl;
+    char* buffer;
+    size_t size;
+};
---
+++ src/simpleGET.cpp
@@ -0,0 +1,54 @@
+#include "simpleGET.h"
+#include <cstdlib>
+#include <cstring>
+#include <curl/curl.h>
+#include <curl/easy.h>
+
+SimpleGET::SimpleGET(const char* url)
+{
+    curl = curl_easy_init();
+
+    curl_easy_setopt(curl, CURLOPT_URL, url);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, this->write_callback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)this); // This _should_ work
+}
+
+SimpleGET::~SimpleGET()
+{
+    free(buffer);
+    curl_easy_cleanup(curl);
+}
+
+CURLcode SimpleGET::Perform()
+{
+    return curl_easy_perform(curl);
+}
+
+char* SimpleGET::GetBuffer()
+{
+    return buffer;
+}
+
+size_t SimpleGET::GetSize()
+{
+    return size;
+}
+
+size_t SimpleGET::write_callback(char* ptr, size_t size, size_t nmemb, void* userdata)
+{
+    size_t realsize = size * nmemb;
+    SimpleGET* current = (SimpleGET*) userdata;
+
+    char* newBuffer = (char*) realloc(current->buffer, current->size + realsize + 1);
+    if (!ptr)
+    {
+        return 0; // OOM
+    }
+
+    current->buffer = newBuffer;
+    memcpy(&(current->buffer[current->size]), ptr, realsize);
+    current->size += realsize;
+    current->buffer[current->size] = 0;
+
+    return realsize;
+}
